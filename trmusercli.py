fd95ad8e24f86dc6a4de3e3191b64fe5081debeb
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-07-31 19:02:56 +0200

    Podstawowa funkcjonalność

--- trmusercli.py
+++ trmusercli.py
@@ -1,4 +1,21 @@
-import raw
+# -*- coding: utf-8 -*-
 import mechanize
 import argparse
+import getpass
 
+br = mechanize.Browser()
+br.open("https://trm24.pl/panel-trm/index.jsp")
+br.select_form(name="login")
+#lang: wpiszidklienta
+AjDi = raw_input("Wpisz ID klienta: ")
+br["clientId"] = AjDi
+#lang: wpiszhaslo
+Paslord = getpass.getpass("Wpisz hasło: ")
+br["clientPass"] = Paslord
+odpoa = br.submit()
+
+assert br.viewing_html()
+print br.title()
+print odpoa.geturl()
+print odpoa.info()
+print odpoa.read()

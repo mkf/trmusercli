b3a9a36e39c6daf261df06022ef568835e62887b
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-08-01 00:11:54 +0200

    Prace nad listą wypożyczeń

--- trmusercli.py
+++ trmusercli.py
@@ -35,9 +35,10 @@ def zdobadzpolecenie(polecenia):
 assert br.viewing_html()
 print br.title()
 print odpoa.geturl()
-print odpoa.info()
+#print odpoa.info()
 #print odpoa.read()
 corobic = zdobadzpolecenie(polecenia)
+#trza będzie zrobić polecenia w różnych językach
 if corobic == 'stankonta':
 	balansowanie = br.open('https://trm24.pl/panel-trm/balance.jsp')
 	balansowac = balansowanie.read()
@@ -56,9 +57,46 @@ if corobic == 'stankonta':
 		#print balansik
 		#print 'UP balansik ----'
 		#quit()
+	#lang: stantwojegokontawynosi
 	print 'Stan twojego konta na TRM24.pl wynosi: %#.2f PLN' % balansik
 elif corobic == 'wypozyczenia':
-	br.open('https://trm24.pl/panel-trm/borrow.jsp')
+	wypozyczania = br.open('https://trm24.pl/panel-trm/borrow.jsp')
+	#wypozyczanie = wypozyczania.read()
+	#lang: terazwybierzdatypoczatkowaikoncowa
+	print "Teraz wybierz datę początkową i końcową:"
+	br.select_form(name = "")
+	odyear = int(raw_input("Data początkowa: rok [XXXX]: "))
+	odmonth = int(raw_input("Data początkowa: miesiąc [1-12]: "))
+	odday = int(raw_input("Data początkowa: dzień: "))
+	br['data_od'] = "%04d-%02d-%02d" % odyear, odmonth, odday
+	doyear = int(raw_input("Data końcowa: rok: "))
+	domonth = int(raw_input("Data końcowa: miesiąc [1-12]: "))
+	doday = int(raw_input("Data końcowa: dzień: "))
+	br['data_do'] = "%04d-%02d-%02d" % doyear, domonth, doday
+	wypostronka = br.submit()
+	tabwypfull = str(re.search(r'table.*?Rower.*?Slot.*?Koszt.*?/table', wypostronka.read(), re.S).group())
+	tabo1 = str(re.sub(r'table', "", tabwypfull))
+	tabo2 = str(re.sub(r'<', "", tabo1))
+	tabo3 = str(re.sub(r'>', "", tabo2))
+	tabo4 = str(re.sub(r'/', "uu", tabo3))
+	tabo5 = str(re.sub('\n', "", tabo4))
+	tabo6 = str(re.sub(r'thead', "", tabo5))
+	tabi0 = re.findall(r'tr.*?uutr', tabo6, re.S)
+	tabila = []
+	for uuh in tabi0:
+		tabi1 = re.findall(r'th.*?uuth', uuh, re.S)
+		tabulacja = "True"
+		tabila.append(tabi1)
+	print tabila
+elif corobic == 'histtransakcji':
+	historiowanie = br.open('https://trm24.pl/panel-trm/balance.jsp')
+	balansowac = balansowanie.read()
+	balanoss = str(re.search(r'strong.*?PL', str(re.search(r'wynosi.*strong.*?PL', balansowac, re.S).group()), re.S).group())
+	balansik = int(str(re.sub(r'\D', "", balanoss)))/100
+	#lang: stantwojegokontawynosi
+	print 'Stan twojego konta na TRM24.pl wynosi: %#.2f PLN' % balansik
+	#lang: tubedziehisttransakcji
+	print "Tu potem będzie historia transakcji tj. doładowań"
 else:
 	#lang: bladcorobicia
 	print u'Błąd w kwestii informacji co robić'
